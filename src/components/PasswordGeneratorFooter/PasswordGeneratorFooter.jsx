226a1e36958964f02e38c2740894e90538584315
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-02-11 19:08:09 +0330

    Copying to clipboard

--- src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/PasswordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,8 +1,11 @@
 import s from "./style.module.css";
 import copyPaseIcon from "../../assets/copy-paste.svg";
 export function PasswordGeneratorFooter(props) {
+  const saveInClipboard = () => {
+    navigator.clipboard.writeText(props.password);
+  };
   return (
-    <div className={s.root}>
+    <div className={s.root} onClick={saveInClipboard}>
       <div>{props.password}</div>
       <img className={s.icon} src={copyPaseIcon} alt="Copy pase icon" />
     </div>
